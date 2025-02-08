ff9438cec4cb8601d1b7358082910077557de8f0
vc <justvisiting@users.noreply.github.com>
2025-02-08 17:07:59 +0530

    Fixed scrolling issue on debug tab

--- android-man/app/src/main/java/com/example/speechapp/DebugLogFragment.java
+++ android-man/app/src/main/java/com/example/speechapp/DebugLogFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,6 +12,7 @@ import androidx.fragment.app.Fragment;
 
 public class DebugLogFragment extends Fragment {
     private TextView debugLogView;
+    private ScrollView scrollView;
     private StringBuilder logBuilder = new StringBuilder();
 
     @Nullable
@@ -18,6 +20,7 @@ public class DebugLogFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_debug, container, false);
         debugLogView = view.findViewById(R.id.debugLogView);
+        scrollView = view.findViewById(R.id.scrollView);
         return view;
     }
 
@@ -41,9 +44,10 @@ public class DebugLogFragment extends Fragment {
 
     private void updateLogDisplay() {
         if (debugLogView != null && isAdded()) {
-            requireActivity().runOnUiThread(() -> 
-                debugLogView.setText(logBuilder.toString())
-            );
+            requireActivity().runOnUiThread(() -> {
+                debugLogView.setText(logBuilder.toString());
+                scrollView.post(() -> scrollView.fullScroll(ScrollView.FOCUS_DOWN));
+            });
         }
     }
 }
